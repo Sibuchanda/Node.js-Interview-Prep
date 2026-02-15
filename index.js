6075744b80c158edb6e846d3797aba9a9d29c082
Sibuchanda <sibuchanda457@gmail.com>
2026-02-15 11:57:11 +0530

    Create an endpoint that accepts JSON data and saves it to MongoDB

--- index.js
+++ index.js
@@ -1,58 +1,41 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './model/connectDB.js';
+import User from './model/model.js';
 
+const DB_URL=process.env.MONGO_URI;
 const PORT = 5000;
 
-let users = [
-    {id: 1, name: "Lester", age: 23}
-]
+
 
 const app = express();
 app.use(express.json()); // Middleware that reads JSON data sent by client and converts it into JavaScript object so we can access it using req.body
 
+//Database connection
+connectDB(DB_URL);
+
+
 app.get('/',(req,res)=>{
     res.send("This is home page");
-})
+});
+
 // Create operation
-app.post('/users', (req,res)=>{
+app.post('/users', async (req,res)=>{
     const newuser = {
-        id : Date.now(),
         name: req.body.name,
         age: req.body.age
     }
-    users.push(newuser);
+    await User.create(newuser);
     res.status(201).send("User added successfully.");
 });
 
 // Read operation
-app.get('/users',(req,res)=>{
+app.get('/users', async (req,res)=>{
+    const users = await User.find({});
     res.status(200).send(users);
 });
 
-//Update operation
-app.put('/users/:id',(req,res)=>{
-    const userId = Number(req.params.id);
-    users=users.map((u)=>{
-        if(u.id===userId){
-            return {
-                ...u,
-                name: req.body.name,
-                age: req.body.age
-            }
-        }else return u;
-    });
-   res.status(200).send("User updated successfully");
-})
-
-//Delete operation
-app.delete('/users/:id',(req,res)=>{
-    const userId=Number(req.params.id);
-    users=users.filter((user)=>{
-        if(user.id!==userId){
-            return {...user}
-        }
-    });
-    res.status(200).send("User deleted successfully");
-})
 
 app.listen(PORT,()=>{
     console.log(`Listening to PORT:${PORT}`);
