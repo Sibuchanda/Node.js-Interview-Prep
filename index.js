02d40719cecd2346b8ea26caf106b92453c07188
Sibuchanda <sibuchanda457@gmail.com>
2026-02-15 09:22:02 +0530

    Build a REST API using Express with CRUD operations

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-import http from 'http';
+import express from 'express';
 
 const PORT = 5000;
 
-const server = http.createServer((req,res)=>{
+let users = [
+    {id: 1, name: "Lester", age: 23}
+]
 
-    if(req.url==='/'){
-        res.end("Home page")
-    }else if(req.url==='/getdata'){
-          const data = {
-            name: "Lester_101",
-            course: "MCA",
-            age: 23
-        };
-       res.writeHead(200,{"Content-Type": "application/json"});
-       res.end(JSON.stringify(data));  // As res.end() accepts only (i) string (ii)Buffer (iii) unit8array .  If we simply pass res.end(data)  then we are actually passing object that is wrong. So we have to convert this object into string. That is why we use JSON.stringfy(). Because it converts the object into JSON and JSON itselt is a string.
+const app = express();
+app.use(express.json()); // Middleware that reads JSON data sent by client and converts it into JavaScript object so we can access it using req.body
+
+app.get('/',(req,res)=>{
+    res.send("This is home page");
+})
+// Create operation
+app.post('/users', (req,res)=>{
+    const newuser = {
+        id : Date.now(),
+        name: req.body.name,
+        age: req.body.age
     }
-    else if(req.url==='/about'){
-        res.writeHead(200,{"Content-Type" : "text/plain"});
-        res.end("This is about page");
-    }else{
-        res.writeHead(404, {"Content-Type": "text/plain"});
-        res.end("404 page not found!");
-    }
+    users.push(newuser);
+    res.status(201).send("User added successfully.");
 });
 
-server.listen(PORT, ()=>{
-    console.log(`Listening to port: ${PORT}`);
+// Read operation
+app.get('/users',(req,res)=>{
+    res.status(200).send(users);
+});
+
+//Update operation
+app.put('/users/:id',(req,res)=>{
+    const userId = Number(req.params.id);
+    users=users.map((u)=>{
+        if(u.id===userId){
+            return {
+                ...u,
+                name: req.body.name,
+                age: req.body.age
+            }
+        }else return u;
+    });
+   res.status(200).send("User updated successfully");
+})
+
+//Delete operation
+app.delete('/users/:id',(req,res)=>{
+    const userId=Number(req.params.id);
+    users=users.filter((user)=>{
+        if(user.id!==userId){
+            return {...user}
+        }
+    });
+    res.status(200).send("User deleted successfully");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Listening to PORT:${PORT}`);
 })
